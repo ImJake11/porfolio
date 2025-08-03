a172c9bb282c3abb8e77571862eac5bcb77accf8
ImJake11 <162854631+ImJake11@users.noreply.github.com>
2025-08-03 23:09:19 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,12 @@
 import AboutMeSection from "./ui/AboutMeSection";
 import HeroSection from "./ui/HeroSection";
-import { Poppins } from "next/font/google";
 import SkillsSection from "./ui/SkillsSection";
 import ProjectsSection from "./ui/ProjectsSection";
 import { Contact } from "lucide-react";
 import ContactSection from "./ui/ContactSection";
 import Footer from "./ui/Footer";
 
-const poppins = Poppins({
-  weight: ["500", "600", "700", "800", "900"],
-  preload: true,
-})
+
 export default function Home() {
   return (
     <div
